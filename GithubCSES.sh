dcbca5b0a770bba9403adcbfb26a3145bad32b46
Aryan K <101513901+aryanbk@users.noreply.github.com>
2024-03-10 19:18:20 +0530

    added/updated MoneySums.java, GithubCSES.sh

--- GithubCSES.sh
+++ GithubCSES.sh
@@ -7,18 +7,25 @@ git add .
 # changes=$(git diff --name-only --cached)
 changes=$(git diff --name-only --cached | grep -v '/\..*')
 
-commit_msg="added "
+commit_msg="added/updated "
 for file in $changes; do
     commit_msg+="$(basename $file), "
 done
 
 commit_msg=${commit_msg::-2}
 
-commit_msg+=" and updated "
-for file in $changes; do
-    commit_msg+="$(basename $file), "
-done
-commit_msg=${commit_msg::-2}
+# commit_msg="added "
+# for file in $changes; do
+#     commit_msg+="$(basename $file), "
+# done
+
+# commit_msg=${commit_msg::-2}
+
+# commit_msg+=" and updated "
+# for file in $changes; do
+#     commit_msg+="$(basename $file), "
+# done
+# commit_msg=${commit_msg::-2}
 
 git commit -m "$commit_msg"
 
